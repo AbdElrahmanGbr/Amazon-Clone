b7571bbef1b61ed75b9cec432cac969ea815d852
AbdElrahman Gbr <abdelrahman.gbr.os@gmail.com>
2022-09-19 16:50:21 +0200

    Sub header full responsive.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {MagnifyingGlassIcon, ShoppingCartIcon} from "@heroicons/react/24/outline";
+import {Bars3Icon, MagnifyingGlassIcon, ShoppingCartIcon} from "@heroicons/react/24/outline";
 
 function Header() {
     return (<header>
@@ -38,7 +38,17 @@ function Header() {
         </div>
 
         {/*Bottom nav*/}
-        <div>
+        <div className={"flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl-6"}>
+            <p className={"link flex items-center"}><Bars3Icon className={"h-6 mr-1"}/> All</p>
+            <p className={"link"}>Prime Video</p>
+            <p className={"link"}>Amazon Business</p>
+            <p className={"link"}>Today's Deals</p>
+            <p className={"link hidden lg:inline-flex"}>Electronics</p>
+            <p className={"link hidden lg:inline-flex"}>Food & Grocery</p>
+            <p className={"link hidden lg:inline-flex"}>Prime</p>
+            <p className={"link hidden lg:inline-flex"}>Buy Again</p>
+            <p className={"link hidden lg:inline-flex"}>Shopper Toolkit</p>
+            <p className={"link hidden lg:inline-flex"}>Health & Personal Care</p>
 
         </div>
 
